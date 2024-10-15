118e1033518818b493b46e8e42c2bc702932d89e
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-10-15 16:05:54 +0300

    done with task 2

--- task 2/Program.cs
+++ task 2/Program.cs
@@ -4,6 +4,18 @@ class Task
 {
     static void Main()
     {
+        Action<string[]> addSir = (strings) =>
+        {
+            foreach(string name in strings)
+            {
+                Console.WriteLine($"Sir, {name}");
+            }
+        };
+
+        string[] strings = Console.ReadLine().Split();
+
+        Line();
+        addSir(strings);
 
     }
     public static void Line()
