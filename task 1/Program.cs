02e96143afa67b958520c9c68ed55587111965c7
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-10-15 16:04:01 +0300

    Добавьте файлы проекта.

---
+++ task 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Task
+{
+    static void Main()
+    {
+        Action<string[]> showStrings = (strings) =>
+        {
+            foreach(string name in strings)
+            {
+                Console.WriteLine(name);
+            }
+        };
+        string[] strings = Console.ReadLine().Split();
+
+        Line();
+        showStrings(strings);
+    }
+    public static void Line()
+    {
+        Console.WriteLine("=======================================");
+    }
+}
