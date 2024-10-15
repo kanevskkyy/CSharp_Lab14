12a5de2c33279a165dad5ffd62ff93c406d598e4
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-10-15 16:13:04 +0300

    done with task 3

---
+++ task 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Task
+{
+    static void Main()
+    {
+        Func<int[], int> calculateMin = (array) =>
+        {
+            int min = array[0];
+            for (int i = 0; i < array.Length; i++) 
+            {
+                if(min > array[i]) min = array[i];
+            }
+            return min;
+        };
+
+        string[] numbers = Console.ReadLine().Split();
+        int[] array = new int[numbers.Length];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            array[i] = int.Parse(numbers[i]);
+        }
+        Line();
+        Console.WriteLine($"Min number = {calculateMin(array)}");
+    }
+    public static void Line()
+    {
+        Console.WriteLine("=======================================");
+    }
+}
