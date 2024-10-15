4bc86cf194cdbf0d86b1f16d4926e1a1923234ae
a_kanevskiy <oleksandrkanovskij@gmail.com>
2024-10-15 16:24:31 +0300

    done with task 4, start 5

---
+++ task 4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Task
+{
+    static void Main()
+    {
+        Predicate<int> oddNumber = (number) => number % 2 == 1;
+        Predicate<int> evenNumber = (number) => number % 2 == 0;
+
+        Console.Write("Start number = ");
+        int start = int.Parse(Console.ReadLine());
+        Console.Write("End number = ");
+        int end = int.Parse(Console.ReadLine());
+        Console.Write("Even or odd = ");
+        string choice = Console.ReadLine();
+        Line();
+
+        switch (choice.ToLower())
+        {
+            case "even":
+                for(int i = start; i <= end; i++)
+                {
+                    if (evenNumber(i)) Console.Write(i + " ");
+                }
+                break;
+            case "odd":
+                for (int i = start; i <= end; i++)
+                {
+                    if (oddNumber(i)) Console.Write(i + " ");
+                }
+                break;
+            default:
+                Console.WriteLine("Invalid choice!");
+                break;
+        }
+        Console.WriteLine();
+        Line();
+    }
+    public static void Line()
+    {
+        Console.WriteLine("=======================================");
+    }
+}
---
+++ task 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Task
+{
+    static void Main()
+    {
+        Func<int[], int[]> addOne = (array) =>
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] += 1;
+            }
+            return array;
+        };
+
+        Func<int[], int[]> multipy
+
+
+    }
+    public static void Line()
+    {
+        Console.WriteLine("=======================================");
+    }
+}
